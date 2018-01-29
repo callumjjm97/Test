ba872e878b1d585b7078c5e69337653bc826f016
callumjjm97 <callumjm97@gmail.com>
2018-01-29 16:22:17 +0000

    Revert "Check what the users larst name is"
    This reverts commit 59deb6ab294ebb96412370b1df6a939be152a7bd.

--- Test.py
+++ Test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+<<<<<<< HEAD
 Name = raw_input("What's your name")
 
 print("Hello " + Name)
@@ -7,3 +8,6 @@ print("Hello " + Name)
 Surname = raw_input("What's your surname?")
 
 print("Hello " + Name + " " + Surname)
+=======
+print("Hello world")
+>>>>>>> parent of 59deb6a... Check what the users larst name is
